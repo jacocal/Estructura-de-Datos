887a0bbfa754b3913ad3ea37e4676a2056804417
legalgui <sebastian.galguera@live.com>
2015-02-12 18:20:12 -0600

    Create Tarea 2 Ejercicio 2 Ordenamiento.h

---
+++ Tarea 2 Ejercicio 2 Ordenamiento.h
@@ -0,0 +1,16 @@
+#ifndef ordenamiento_h
+#define ordenamiento_h
+
+#include <iostream>
+
+class Ordenamiento {
+public:
+    static void burbujaAsc(char arreglo[], int n);
+    static void burbujaDec(char arreglo[], int n);
+    static void insercionAsc(char arreglo[], int n);
+    static void insercionDec(char arreglo[], int n);
+    static void seleccionAsc(char arreglo[], int n);
+    static void seleccionDec(char arreglo[], int n);
+    
+    
+};
