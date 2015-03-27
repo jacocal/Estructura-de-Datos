cea2e809d9282ea956c677d1461caf1168980027
legalgui <sebastian.galguera@live.com>
2015-03-26 21:25:05 -0600

    Create nodo.cpp

---
+++ Tarea-4/Ejercicio3/nodo.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include "nodo.h"
+
+
+template <class T>
+Nodo<T>::Nodo() {
+    this->siguiente = NULL;
+}
+
+template <class T>
+Nodo<T>::Nodo(T info) : info(info) {
+    this->siguiente = NULL;
+}
+
+template <class T>
+T Nodo<T>::getInfo() {
+    return info;
+}
+
+template <class T>
+void Nodo<T>::setInfo(T value) {
+    info = value;
+}
+
+template <class T>
+void Nodo<T>::setSiguiente(Nodo<T> * value) {
+    siguiente = value;
+}
+
+template <class T>
+void Nodo<T>::setAnterior(Nodo<T> * value) {
+    anterior = value;
+}
+
+template <class T>
+Nodo<T> * Nodo<T>::getSiguiente() {
+    return siguiente;
+}
+
+
+template <class T>
+Nodo<T> * Nodo<T>::getAnterior() {
+    return anterior;
+}
+
+template <class T>
+std::ostream& operator <<(std::ostream& os, Nodo<T>& nodo) {
+    os << nodo.info;
+    return os;
+}
+
